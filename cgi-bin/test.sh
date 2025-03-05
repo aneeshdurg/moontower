cbe225ff87c8af2a943d1db9f7e6d231d4f1b2b8
Aneesh Durg <aneeshdurg17@gmail.com>
2025-03-05 20:11:33 +0000

    Experiment with index design

--- cgi-bin/test.sh
+++ cgi-bin/test.sh
@@ -1,37 +1,49 @@
 #!/usr/bin/bash
 
 # Send headers
-echo "Content-Type: text/html"
-echo
-echo
+echo -e "Content-Type: text/html\n\n"
 
 # Send the content!
-echo "<!DOCTYPE html>"
-echo "<html>"
-echo "<head>"
-echo "  <link rel=\"stylesheet\" href=\"style.css\">"
-echo "  <title>MTCC</title>"
-echo "</head>"
+echo "\
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel=\"stylesheet\" href=\"/~aneesh/test/style.css\">
+  <title>MTCC</title>
+</head>
 
-echo "<body>"
-echo "<div class=\"container\">"
+<body>
+<div class=\"container\">
+"
 
-cat /home/root/public/includes/header.html
+echo "  <div class=\"header\">"
+cat /home/aneesh/public/includes/header.html
+cat /home/aneesh/public/includes/description.html
+echo "  </div>"
 
-cat /home/root/public/includes/ethos.html
-
-echo "<h2>Users:</h2>"
-echo "<ul>"
+echo "\
+  <br>
+  <br>
+  <div class=\"userbar\">
+    <h2>Users:</h2>
+    <ul>"
 for dir in $(find /home -maxdepth 1 -type d | grep -v '^/home/root$\|^\/home$'); do
   user=$(basename "$dir")
-  echo "  <li><a href=\"/~${user}\">~/${user}</a></li>"
+  echo "      <li><a href=\"/~${user}\">~/${user}</a></li>"
 done
-echo "</ul>"
+echo "\
+      </ul>
+  </div>"
 
-echo "<footer>"
-echo "  <i>Generated on <span class=\"generateddate\">$(date)</span></i>"
-echo "</footer>"
+cat /home/aneesh/public/includes/ethos.html
 
-echo "</div>"
-echo "</body>"
-echo "</html>"
+echo "\
+  <div class=\"footer\">
+    <br>
+    <footer>
+      <i>Generated on <span class=\"generateddate\">$(date)</span></i>
+    </footer>
+  </div>
+</div>
+</body>
+</html>"
